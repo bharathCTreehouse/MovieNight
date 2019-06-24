bef99af874ca1c4bf8b0904bdaad3db4d48de6c1
Bharath Chandrashekar <bharath.chandrashekar@fluke.com>
2019-06-24 23:16:12 +0530

    Initial commit

---
+++ MovieNight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by Bharath on 24/06/19.
+//  Copyright © 2019 Bharath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
